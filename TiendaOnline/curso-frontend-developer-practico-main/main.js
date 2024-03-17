6272966c5ca8e32c1e63f4c665d049d086cd2b05
Luis nava <lnavaa1500@alumno.ipn.mx>
2024-03-16 22:22:15 -0600

    Agregamos el mobile menu y su funcionalidad de solo aparecer cuando estemos en mobile

--- TiendaOnline/curso-frontend-developer-practico-main/main.js
+++ TiendaOnline/curso-frontend-developer-practico-main/main.js
@@ -1,11 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle( "inactive" );
-    
+}
+
+function toggleMovileMenu() {
+    mobileMenu.classList.toggle("inactive");
 }
 
 
 navEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMovileMenu);
