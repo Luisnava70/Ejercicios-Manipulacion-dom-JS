81bfd8c6d8e5cc96738c7b26a4ea8c35ad5f40b2
Luis nava <lnavaa1500@alumno.ipn.mx>
2024-03-17 00:30:54 -0600

    Agregamos la funcionalidad de abrir el menu mobile, menu del carrito y del perfil del usuario

--- TiendaOnline/curso-frontend-developer-practico-main/main.js
+++ TiendaOnline/curso-frontend-developer-practico-main/main.js
@@ -2,15 +2,45 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle( "inactive" );
+    const isAsideOpen = !aside.classList.contains('inactive');
+    if(isAsideOpen){
+        aside.classList.add( 'inactive' );
+        desktopMenu.classList.toggle("inactive");
+    }else{
+        desktopMenu.classList.toggle("inactive");
+    }
+    /* desktopMenu.classList.toggle( "inactive" ); */
 }
 
 function toggleMovileMenu() {
-    mobileMenu.classList.toggle("inactive");
+    const isAsideOpen = !aside.classList.contains('inactive');
+    if(isAsideOpen){
+        aside.classList.add( 'inactive' );
+        mobileMenu.classList.toggle("inactive");
+    }else{
+        mobileMenu.classList.toggle("inactive");
+    }
+    
 }
 
+function toggleCarritoAside(){
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive'); 
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+    if(isMobileMenuOpen || isDesktopMenuOpen){
+        mobileMenu.classList.add('inactive');
+        desktopMenu.classList.add('inactive');
+        aside.classList.toggle("inactive");
+    }else{
+        aside.classList.toggle("inactive");
+    }
+
+    /*  */
+}
 
 navEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMovileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
