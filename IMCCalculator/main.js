465760d3637cd4f0a783570855050855187a0647
Luis nava <lnavaa1500@alumno.ipn.mx>
2024-05-13 13:47:45 -0600

    Agregamos la primera version de nuestra app para calcular el IMC

---
+++ IMCCalculator/main.js
@@ -0,0 +1,121 @@
+const btnFemmale = document.querySelector('.female');
+const btnMale = document.querySelector('.male');
+const clientHeight = document.querySelector('.clients-height');
+const inputRange = document.getElementById('height');
+const subtractWeight = document.querySelector('.decreast-weight');
+const addWeight = document.querySelector('.increase-weight')
+const clientWeight = document.querySelector('.clients-weight');
+const clientAge = document.querySelector('.clients-age');
+const subtracAge = document.querySelector('.decreast-age');
+const addAge = document.querySelector('.increase-age');
+const btnCalculateImc = document.querySelector('.calculate-imc');
+const resultsView = document.querySelector('.view-result');
+const attributeView = document.querySelector('.attribute-view');
+const pResult = document.querySelector('.result-imc');
+const pStatusBmi = document.querySelector('.status-imc');
+const backToCalculate = document.querySelector('.back-to-calculate');
+
+let height = 170;
+let weight = 30;
+let age = 20;
+
+
+
+function selectGender(event){
+    if(event.target.alt === 'female'){
+        btnFemmale.classList.add('genderSelected');
+        btnMale.classList.remove('genderSelected');
+    }else if(event.target.alt === 'male'){
+        btnMale.classList.add('genderSelected');
+        btnFemmale.classList.remove('genderSelected');
+    }
+}
+
+function reduceWeight(){
+    weight--;
+    clientWeight.innerHTML = weight;
+}
+
+function increaseWeight(){
+    weight++;
+    clientWeight.innerHTML = weight;
+}
+
+function reduceAge(){
+    age--;
+    clientAge.innerHTML = age;
+}
+
+function increaseAge(){
+    age++;
+    clientAge.innerHTML = age;
+}
+
+
+
+
+    inputRange.addEventListener('input', ()=> {
+    clientHeight.textContent = inputRange.value + " cm";
+    height = Number(inputRange.value);
+    console.log(height);
+    });
+
+    function selectStatus(height,weight){
+        const bmiValue = calculateBmi(height,weight);
+        if(bmiValue < 18.5){
+            pStatusBmi.textContent = "Underweight";
+            pStatusBmi.style.color = "#2E86C1";
+        }
+        if(bmiValue < 24.9 && bmiValue > 18.5){
+            pStatusBmi.textContent = "Normal";
+            pStatusBmi.style.color = "#2ECC71";
+        }
+    
+        if(bmiValue > 24.9 && bmiValue < 29.9){
+            pStatusBmi.textContent = "Overweight";
+            pStatusBmi.style.color = "#F39C12";
+        }
+    
+        if(bmiValue > 30){
+            pStatusBmi.textContent = "Obese";
+            pStatusBmi.style.color = "#E74C3C";
+        }
+    }
+
+    function assignValues(height,weight){
+        const bmi = calculateBmi(height,weight);
+        pResult.innerHTML = bmi;
+    }
+    
+    function calculateBmi(height,weight){
+        const bmi = Number (weight / ((height/100) * (height/100)));
+        return bmi.toFixed(2);
+    }
+
+function goToCalculateImc(){
+    attributeView.classList.add('inactive');
+    resultsView.classList.remove('inactive');
+    assignValues(height,weight);
+    selectStatus(height,weight)
+}
+
+function backToCalculateBmi(){
+    attributeView.classList.remove('inactive');
+    resultsView.classList.add('inactive');
+}
+
+
+console.log(height,weight);
+
+backToCalculate.addEventListener('click',backToCalculateBmi);
+btnFemmale.addEventListener('click',selectGender);
+btnMale.addEventListener('click',selectGender);
+subtractWeight.addEventListener('click',reduceWeight);
+addWeight.addEventListener('click',increaseWeight);
+subtracAge.addEventListener('click',reduceAge)
+addAge.addEventListener('click',increaseAge);
+btnCalculateImc.addEventListener('click',goToCalculateImc);
+
+
+
+
---
+++ IMCCalculator/results.js
@@ -0,0 +1,30 @@
+
+/* bmiSValues.push({
+    "bmi": "Underweight",
+    "color": "#2E86C1"
+});
+bmiSValues.push({
+    
+    "bmi": "Normal",
+    "color": "#2ECC71"
+})
+bmiSValues.push({
+    
+    "bmi": "Overweight",
+    "color": "#F39C12"
+})
+bmiSValues.push({
+    "bmi": "Obese",
+    "color": "#E74C3C"
+}) */
+
+
+
+
+
+
+
+
+
+
+
